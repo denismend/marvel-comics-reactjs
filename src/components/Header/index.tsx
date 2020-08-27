900739866f3dadadea0cd308d0b7a07567e216cd
denismend <denisladeira1@gmail.com>
2020-08-27 13:40:22 -0300

    add characters card

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Container } from './styles';
+
 const Header: React.FC = () => (
   <Container>
     <div className="row">
-      <div className="col-sm-6 col-xs-12">
-        <Link to="/">
-          <img
-            className="logo"
-            height="40"
-            src="/img/MarvelLogoAPI.svg"
-            role="presentation"
-          />
-        </Link>
-      </div>
+      <div className="col-sm-6 col-xs-12" />
     </div>
   </Container>
 );
